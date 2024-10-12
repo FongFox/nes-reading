0afcb150831302e5a9baba2486f46612f866dd3c
FongFox <phong.tgn.coder@gmail.com>
2024-10-12 13:43:09 +0700

    1. Init readme file.

--- src/main/java/com/admin/nesreading/repository/CartRepository.java
+++
@@ -1,9 +0,0 @@
-package com.admin.nesreading.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.admin.nesreading.domain.Cart;
-
-public interface CartRepository extends JpaRepository<Cart, Long> {
-
-}
